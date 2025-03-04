2fcdd2bc2a47d6cadca3ac96f854148a15b4263e
Sophie Bunge <sophie.bunge@hotmail.com>
2025-03-04 13:20:33 +0100

    Removed embedded Git repository (backend)

---
+++ frontend/AvatarReactApp/src/App.jsx
@@ -0,0 +1,15 @@
+// src/App.js
+import React from 'react';
+import './App.css';
+import EmotionCapture from './EmotionCapture';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Real-Time Emotion Detection</h1>
+      <EmotionCapture />
+    </div>
+  );
+}
+
+export default App;
