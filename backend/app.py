ae3ce7b52e91a23f4a990ec937e70902ae6806f4
Sophie Bunge <sophie.bunge@hotmail.com>
2025-03-14 22:11:14 +0100

    chache force

--- backend/app.py
+++ backend/app.py
@@ -25,5 +25,5 @@ def detect_emotion():
         return jsonify({'error': str(e)}), 500
 
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 10000))
+    port = int(os.environ.get("PORT", 5000))
     app.run(host='0.0.0.0', port=port)
