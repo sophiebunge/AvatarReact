3ea29640653e31988e5588debc76cb05b49b11a5
Sophie Bunge <sophie.bunge@hotmail.com>
2025-03-14 22:27:52 +0100

    port update

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: '0.0.0.0', // Bind to all interfaces
+    port: process.env.PORT || 5173, // Use the port provided by Render or fallback to 5173
+  },
 })
