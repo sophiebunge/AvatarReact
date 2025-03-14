9b1e7141badc5c2fa767c05e49be05417541743f
Sophie Bunge <sophie.bunge@hotmail.com>
2025-03-14 22:02:49 +0100

    new node version

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -15,7 +15,7 @@ const App = () => {
   const captureAndSendImage = async () => {
     const imageSrc = webcamRef.current.getScreenshot();
     try {
-      const response = await axios.post("http://localhost:3000/emotion", {
+      const response = await axios.post("https://avatarreact.onrender.com", {
         image: imageSrc,
       });
       if (response.data.emotion) {
